8cd369b94b08897315b83a5150c86537ec40e83e
SayAn1-dls <sayanbhatt2005@gmail.com>
2026-01-29 10:57:00 +0530

    study: embedded systems interrupt handling [20]

--- snippets/templates.py
+++ snippets/templates.py
@@ -21,3 +21,4 @@
 - [2026-01-25 040] study: quantum entanglement chapter notes
 - [2026-01-25 060] research: links on transformer attention
 - [2026-01-26 020] study: embedded systems interrupt handling
+- [2026-01-29 020] study: embedded systems interrupt handling
