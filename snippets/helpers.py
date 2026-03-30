9ee999d0b3d092700da5e1fa241cf5e009c1d73b
SayAn1-dls <sayanbhatt2005@gmail.com>
2026-03-30 13:54:00 +0530

    docs: document API endpoints [39]

--- snippets/helpers.py
+++ snippets/helpers.py
@@ -86,3 +86,4 @@
 - [2026-03-29 039] docs: document API endpoints
 - [2026-03-29 059] chore: update config and env schema
 - [2026-03-30 019] docs: update deployment guide
+- [2026-03-30 039] docs: document API endpoints
