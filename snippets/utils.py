ca2e2fce15f7f5cd27018b40ac151c5e39277090
SayAn1-dls <sayanbhatt2005@gmail.com>
2026-05-25 19:51:00 +0530

    feat: add rate limiting middleware [18]

--- snippets/utils.py
+++ snippets/utils.py
@@ -140,3 +140,4 @@
 - [2026-05-22 018] feat: add rate limiting middleware
 - [2026-05-23 018] feat: add rate limiting middleware
 - [2026-05-24 018] feat: add rate limiting middleware
+- [2026-05-25 018] feat: add rate limiting middleware
