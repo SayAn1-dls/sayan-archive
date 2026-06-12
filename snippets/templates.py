602aa5e048503ea9aef779af172645b02e7ccb01
SayAn1-dls <sayanbhatt2005@gmail.com>
2026-06-12 19:57:00 +0530

    test: add unit tests for parser [80]

--- snippets/templates.py
+++ snippets/templates.py
@@ -136,3 +136,4 @@
 - [2026-06-12 020] study: embedded systems interrupt handling
 - [2026-06-12 040] study: quantum entanglement chapter notes
 - [2026-06-12 060] research: links on transformer attention
+- [2026-06-12 080] test: add unit tests for parser
