44820b2927ac9e6127510e0a0aa1464bf9701d6e
SayAn1-dls <sayanbhatt2005@gmail.com>
2026-06-12 13:54:00 +0530

    docs: document API endpoints [39]

--- snippets/helpers.py
+++ snippets/helpers.py
@@ -147,3 +147,4 @@
 - [2026-06-11 019] docs: update deployment guide
 - [2026-06-11 039] docs: document API endpoints
 - [2026-06-12 019] docs: update deployment guide
+- [2026-06-12 039] docs: document API endpoints
