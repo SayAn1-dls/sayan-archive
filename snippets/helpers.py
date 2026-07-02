fc6409cc6e5a852bf0945cb6c286ece73cb1ae36
SayAn1-dls <sayanbhatt2005@gmail.com>
2026-07-02 21:54:00 +0530

    chore: update config and env schema [59]

--- snippets/helpers.py
+++ snippets/helpers.py
@@ -161,3 +161,4 @@
 - [2026-06-27 019] docs: update deployment guide
 - [2026-07-02 019] docs: update deployment guide
 - [2026-07-02 039] docs: document API endpoints
+- [2026-07-02 059] chore: update config and env schema
