9636eb3a5378e4ee3d57723759400b7940f15c3e
SayAn1-dls <sayanbhatt2005@gmail.com>
2026-07-07 19:51:00 +0530

    feat: add webhook support to alerting [58]

--- snippets/utils.py
+++ snippets/utils.py
@@ -174,3 +174,4 @@
 - [2026-07-02 058] feat: add webhook support to alerting
 - [2026-07-07 018] feat: add rate limiting middleware
 - [2026-07-07 038] feat: add retry logic for failed jobs
+- [2026-07-07 058] feat: add webhook support to alerting
